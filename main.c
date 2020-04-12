b02eda3a3f3cf92e431c501eef11823c28c1303a
Achmad Fawaz Ferdiansyah <47052717+Herpeto@users.noreply.github.com>
2020-04-12 21:49:26 +0700

    membuat inputan nGram & memasukkan fungsi

--- main.c
+++ main.c
@@ -1,9 +1,50 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "HashCode/hashCode.h"
+#include <stdio.h>
+#include <stdlib.h>
 
-int main(){
+#include "HashCode/hashCode.h"
+#include "parser.h"
+#include "mencari.h"
+
+void menu();
+
+int main()
+{
+    // List Variabel
     table *pasanganTable;
+    static char arr[250000][50];
+    int nGram;
+    
+    
+    //Implementasi Algoritma
+    //Menu
+    printf("Selamat datang dalam program nGram");
+    
+    //Penggunaan fungsi input file dan fungsi parser dari file "parser.h"
+    FILE* stream = readFile();
+    while(stream != NULL)
+    {
+	    parser(stream, arr);
+        
+        printf("Masukkan nilai nGram : ");
+        scanf("%d",&nGram);
+        while(nGram < 2) //Input checker untuk nGram < 2, karena dibawah 2 akan = kata random semua
+        {
+            printf("nGram harus diatas 2!");
+            printf("Masukkan nilai nGram : ");
+            scanf("%d",&nGram);
+        }
+        
+        
+        
+    }
     
     return 0;
 }
+
+/*
+void menu()
+{
+    
+    
+}
+*/
