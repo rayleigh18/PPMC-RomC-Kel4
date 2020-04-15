b87082ef9bd5b065477e58ae2af7b5866c8e51df
odelia-062 <odelia@users.noreply.github.com>
2020-04-15 21:12:30 +0700

    Update parser.c
    headline

--- Parser/parser.c
+++ Parser/parser.c
@@ -1,3 +1,12 @@
+/* EL2208 Praktikum Pemecahan Masalah dengan C 2019/2020
+* MODUL 8 – TUGAS BESAR
+* Kelompok : 4
+* Hari dan Tanggal : Rabu, 15 April 2020
+* Asisten (NIM) : Hamdani Fadhli (13217058)
+* Nama File : parser.c
+* Deskripsi : Parsing kata dari file eksternal
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
