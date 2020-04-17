def4e6f7cbc0a0e2d3fac637a51b1e85583ab2c7
Herpeto <herpeto.remington@gmail.com>
2020-04-17 17:05:44 +0700

    Update mencari.c

--- Mencari/mencari.c
+++ Mencari/mencari.c
@@ -79,10 +79,12 @@ void process(table *pasanganTable, int nGram, FILE *out, int totalKata)
                 countKata++;
             }
             else{
+				int  randomNumb1 = rand () % (pasanganTable->size+1);
                 char first[10000];
-                for (int i = 0; i < SIZE_TABLE; i++){
-                    if (pasanganTable->list[i]!= NULL){
-                        strcpy(first , pasanganTable->list[i]->key);
+                for (int i = randomNumb1; i < SIZE_TABLE + randomNumb1; i++){
+                    if (pasanganTable->list[abs(i%SIZE_TABLE)]!= NULL){
+                        strcpy(first , pasanganTable->list[abs(i%SIZE_TABLE)]->key);
+                        break;
                     }
                 }
                 char *temp = strtok(first," ");
